8a631a72dabbbc0fb847c76da5d00108eb61fd3e
felipemaciel <felipemrvieira@gmail.com>
2018-05-28 10:54:05 -0300

    edit profile and article restrictions

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   resources :users, except: [:new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :articles
-  root 'articles#index'
+  root 'pages#home'
+
+  get 'login', to: 'sessions#new' 
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
